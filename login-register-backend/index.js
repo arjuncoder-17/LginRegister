b6047044eeb58a3490d67217a6b14dd4463e4f68
Arjun <arjun.20212039@mnnit.ac.in>
2023-06-19 16:22:06 +0530

    first commit

---
+++ login-register-backend/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import mogoose from "mongoose"
+import bodyParser from "body-parser"
+
+const app=express();
+ app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+mongoose.connect("mongodb://localhost:27017/myLoginRegisterDB",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+},()=>{
+  console.log("DB connected");
+})
+
+//Routes
+app.get("/",(req,res)=>{
+  res.send("My API")
+})
+
+app.listen(9002,()=>{
+  comsole.log("BE started at port 9002")
+})
